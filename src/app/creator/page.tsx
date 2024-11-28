810aab8e3ba0d14e1a34bdf63c2573b49ef808ef
Anioke sebastian <aniokechukwudi7@gmail.com>
2024-11-28 01:31:23 +0100

    add page favicon

--- src/app/creator/page.tsx
+++ src/app/creator/page.tsx
@@ -1,17 +1,22 @@
+"use client";
 import CreateTokenUI from "@/components/CreateTokenUI";
 import Footer from "@/components/Footer";
 import TokenDetailsUI from "@/components/TokenDetailsUI";
 
 function CreatorPage() {
 	return (
-		<form className='mt-10'>
-			<h1 className='text-[1.6rem] text-gray-300 mb-4'>Create Token</h1>
-			<div className='grid grid-cols-2   gap-10'>
-				<CreateTokenUI />
-				<TokenDetailsUI />
-			</div>
-			<Footer className='py-8' />
-		</form>
+		<div>
+			<form className='mt-10'>
+				<h1 className='text-[1.6rem] text-gray-300 mb-4'>
+					Create Token
+				</h1>
+				<div className='grid grid-cols-2   gap-10'>
+					<CreateTokenUI />
+					<TokenDetailsUI />
+				</div>
+				<Footer className='py-8' />
+			</form>
+		</div>
 	);
 }
 export default CreatorPage;
